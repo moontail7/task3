d171b551686d4de9ea6d889215106e8759520ece
Brendan Oates <brendanflight@gmail.com>
2023-10-30 17:20:07 +1000

    database methods to display data

--- website/views.py
+++ website/views.py
@@ -1,7 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import Event
+from . import db
+from sqlalchemy import and_
 
-main_bp = Blueprint('main', __name__)
+# Create a Blueprint for the main part of the application
+mainbp = Blueprint('main', __name__)
 
-@main_bp.route('/')
+# Route to display the main index page
+@mainbp.route('/')
 def index():
-    return '<h1>Starter code for the assessment<h1>'
+    # Retrieve a list of events from the database
+    events = db.session.scalars(db.select(Event)).all()
+    return render_template('index.html', events=events)
+
+@mainbp.route('/search')
+def search():
+    if 'search' in request.args and request.args['search']:
+        query = "%" + request.args['search'] + "%"
+        # Create a query with a filter condition
+        events = db.session.query(Event).filter(Event.description.like(query)).all()
+        return render_template('index.html', events=events)
+    else:
+        return redirect(url_for('main.index'))
