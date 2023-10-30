cf77a87e3bdf9edbe3b493f6be40503ad8ef9c5c
moontail7 <n11581361@qut.edu.au>
2023-10-30 17:12:38 +1000

    auth

--- website/auth.py
+++ website/auth.py
@@ -1,33 +1,69 @@
 from flask import Blueprint, flash, render_template, request, url_for, redirect
-from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+from .forms import LoginForm, RegisterForm
+from flask_login import login_user, login_required, logout_user
 from .models import User
-from .forms import LoginForm,RegisterForm
-from flask_login import login_user, login_required,logout_user
 from . import db
 
-# Create a blueprint - make sure all BPs have unique names
-auth_bp = Blueprint('auth', __name__)
+# Create a Blueprint for authentication
+bp = Blueprint('auth', __name__)
 
-# this is the hint for a login function
-# @auth_bp.route('/login', methods=['GET', 'POST'])
-# def authenticate(): #view function
-#     login_form = LoginForm()
-#     error=None
-#     if(login_form.validate_on_submit()==True):
-#         user_name = login_form.user_name.data
-#         password = login_form.password.data
-#         u1 = User.query.filter_by(name=user_name).first()
-#         if u1 is None:
-#             error='Incorrect user name'
-#         elif not check_password_hash(u1.password_hash, password): # takes the hash and password
-#             error='Incorrect password'
-#         if error is None:
-#             login_user(u1)
-#             nextp = request.args.get('next') #this gives the url from where the login page was accessed
-#             print(nextp)
-#             if next is None or not nextp.startswith('/'):
-#                 return redirect(url_for('index'))
-#             return redirect(nextp)
-#         else:
-#             flash(error)
-#     return render_template('user.html', form=login_form, heading='Login')
+# Register function for user registrationg
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    # Create a RegisterForm instance
+    register = RegisterForm()
+    
+    if register.validate_on_submit() == True:
+        uname = register.user_name.data
+        pwd = register.password.data
+        email = register.email_id.data
+        
+        # Check if the username already exists
+        user = db.session.scalar(db.select(User).where(User.name == uname))
+        if user:
+            flash('User name already exists, please login')
+            return redirect(url_for('auth.login'))
+        
+        # Generate a password hash
+        pwd_hash = generate_password_hash(pwd)
+        
+        # Create a new user and add them to the database
+        new_user = User(name=uname, password_hash=pwd_hash, emailid=email)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    else:
+        return render_template('user.html', form=register, heading='Register')
+
+# Login function
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    error = None
+    if login_form.validate_on_submit() == True:
+        user_name = login_form.user_name.data
+        password = login_form.password.data
+        
+        # Query the user by their username
+        user = db.session.scalar(db.select(User).where(User.name == user_name))
+        if user is None:
+            error = "Incorrect user name"
+        elif not check_password_hash(user.password_hash, password):
+            error = "Incorrect password"
+        if error is None:
+            # Log in the user
+            login_user(user)
+            return redirect(url_for('main.index'))
+        else:
+            flash(error)
+    return render_template('user.html', form=login_form, heading='Login')
+
+# Logout function
+@bp.route('/logout')
+@login_required
+def logout():
+    # Log the user out
+    logout_user()
+    # return 'You have been logged out'
+    return render_template('logout.html', heading='Loginout')
