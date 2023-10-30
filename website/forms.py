513bbef1c2fc22ba637002466ab6ab41c7f8fcfd
moontail7 <n11581361@qut.edu.au>
2023-10-30 17:03:29 +1000

    forms

--- website/forms.py
+++ website/forms.py
@@ -1,21 +1,43 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField
-from wtforms.validators import InputRequired, Length, Email, EqualTo
+from wtforms.fields import TextAreaField, SubmitField, IntegerField, StringField, PasswordField, SelectField
+from wtforms.validators import InputRequired, Length, Email, EqualTo, NumberRange
+from flask_wtf.file import FileRequired, FileField, FileAllowed 
 
-#creates the login information
+
+# Define a set of allowed image file extensions
+ALLOWED_FILE = {'PNG', 'JPG', 'png', 'jpg', 'jpeg'}
+
+# Form for creating an event
+class EventForm(FlaskForm):
+    STATUS_LIST = [
+        ('open', 'Open'),
+        ('inactive', 'Inactive'),
+        ('soldout', 'Sold Out'),
+        ('cancelled', 'Event Cancelled'),
+    ]
+    name = StringField('Event Name', validators=[InputRequired()])
+    description = TextAreaField('Event Description', validators=[InputRequired()])
+    venue = TextAreaField('Event Venue', validators=[InputRequired()])
+    date = TextAreaField('Event Date', validators=[InputRequired()])
+    status = SelectField('Event Status', choices=STATUS_LIST)
+    image = FileField('Event Image', validators=[FileRequired(message='Image cannot be empty'), FileAllowed(ALLOWED_FILE, message='File is not supported')])
+    submit = SubmitField("Create")
+
+# Form for user login
 class LoginForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
-    password=PasswordField("Password", validators=[InputRequired('Enter user password')])
+    user_name = StringField("User Name", validators=[InputRequired('Enter user name')])
+    password = PasswordField("Password", validators=[InputRequired('Enter user password')])
     submit = SubmitField("Login")
 
- # this is the registration form
+# Form for user registration
 class RegisterForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired()])
+    user_name = StringField("User Name", validators=[InputRequired()])
     email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
-    #linking two fields - password should be equal to data entered in confirm
-    password=PasswordField("Password", validators=[InputRequired(),
-                  EqualTo('confirm', message="Passwords should match")])
+    password = PasswordField("Password", validators=[InputRequired(), EqualTo('confirm', message="Passwords should match")])
     confirm = PasswordField("Confirm Password")
-
-    #submit button
     submit = SubmitField("Register")
+
+# Form for adding comments
+class CommentForm(FlaskForm):
+    text = TextAreaField('Comment', [InputRequired()])
+    submit = SubmitField('Create')
